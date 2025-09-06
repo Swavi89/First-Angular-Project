a04d830308ccafb3af537b3095647c43ea20ec0e
swavi89 <swaviman89@gmail.com>
2025-09-06 23:54:42 +0530

    styling and use next custom component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { header } from './header/header.component';
+import { User } from "./user/user";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, header],
+  imports: [RouterOutlet, header, User],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
