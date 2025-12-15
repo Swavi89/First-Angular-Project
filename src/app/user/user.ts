5d78bab5b9c5448254a3176a14f621cbd398ad7c
swavi89 <swaviman89@gmail.com>
2025-12-15 23:26:11 +0530

    remove signal & back to previous input typos

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,4 @@
-import { Component, computed, Input, input} from '@angular/core';
+import { Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -8,17 +8,12 @@ import { Component, computed, Input, input} from '@angular/core';
 })
 export class User { 
 
-  // @Input({required: true}) avatar!: string;
-  // @Input({required: true}) name!: string;
-  avatar = input.required<string>();
-  name = input.required<string>()
-  imagePath = computed(() => {
-    return 'assets/users/' + this.avatar();
-  })
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
 
-  // get imagePath() {
-  //   return 'assets/users/' + this.avatar;
-  // }
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
 
   onSelectUser () {
 
